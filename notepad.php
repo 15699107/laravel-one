f3cb536c9e333e72f73193f532795e3ee345e953
ric <596868636@qq.com>
2017-07-14 10:58:00 +0800

    init project

---
+++ notepad.php
@@ -0,0 +1,11 @@
+<?php
+#第一步进到相关目录
+#git init
+#git add .
+#git commit -m'init project'
+#git remote add origin https://github.com/15699107/laravel-one.git
+#git push -u origin master
+#邮箱地址
+#登录密码
+
+###以上是推送到github平台上面###
